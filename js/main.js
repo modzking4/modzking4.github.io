eeeb1ace9759a5b06b0ca2ce030e71773c9c68a3
modzking4 <liamramsden89@gmail.com>
2022-08-18 07:36:34 +0100

    123

--- js/main.js
+++ js/main.js
@@ -3,17 +3,14 @@
  *
  * ------------------------------------------------------------------- */
 
-(function($) {
-
+(function($)
+{
     "use strict";
-    
     var cfg = {
         scrollDuration : 800, // smoothscroll duration
         mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
     },
-
     $WIN = $(window);
-
     // Add the User Agent to the <html>
     // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
     var doc = document.documentElement;
@@ -24,20 +21,17 @@
         $(".header-logo img").attr("src", "images/logo.png");
     }
 
-
    /* Preloader
     * -------------------------------------------------- */
     var ssPreloader = function() {
-        
         $("html").addClass('ss-preload');
-
-        $WIN.on('load', function() {
-
+        $WIN.on('load', function()
+        {
             //force page scroll position to top at page refresh
             $('html, body').animate({ scrollTop: 0 }, 'normal');
-
             // will first fade out the loading animation 
-            $("#loader").fadeOut("slow", function() {
+            $("#loader").fadeOut("slow", function()
+            {
                 // will fade out the whole DIV that covers the website.
                 $("#preloader").delay(300).fadeOut("slow");
             }); 
@@ -45,76 +39,67 @@
             // for hero content animations 
             $("html").removeClass('ss-preload');
             $("html").addClass('ss-loaded');
-        
         });
     };
 
-
    /* Menu on Scrolldown
     * ------------------------------------------------------ */
-    var ssMenuOnScrolldown = function() {
-        
+    var ssMenuOnScrolldown = function() {        
         var menuTrigger = $('.header-menu-toggle');
-
-        $WIN.on('scroll', function() {
-
+        $WIN.on('scroll', function()
+        {
             if ($WIN.scrollTop() > 150) {
                 menuTrigger.addClass('opaque');
             }
             else {
                 menuTrigger.removeClass('opaque');
             }
-
         });
     };
-
 
    /* OffCanvas Menu
     * ------------------------------------------------------ */
     var ssOffCanvas = function() {
-
-        var menuTrigger     = $('.header-menu-toggle'),
-            nav             = $('.header-nav'),
-            closeButton     = nav.find('.header-nav__close'),
-            siteBody        = $('body'),
-            mainContents    = $('section, footer');
-
+        var menuTrigger = $('.header-menu-toggle'),
+        nav = $('.header-nav'),
+        closeButton = nav.find('.header-nav__close'),
+        siteBody = $('body'),
+        mainContents = $('section, footer');
         // open-close menu by clicking on the menu icon
-        menuTrigger.on('click', function(e){
+        menuTrigger.on('click', function(e)
+        {
             e.preventDefault();
             siteBody.toggleClass('menu-is-open');
         });
 
         // close menu by clicking the close button
-        closeButton.on('click', function(e){
+        closeButton.on('click', function(e)
+        {
             e.preventDefault();
             menuTrigger.trigger('click');
         });
 
         // close menu clicking outside the menu itself
-        siteBody.on('click', function(e){
+        siteBody.on('click', function(e)
+        {
             if( !$(e.target).is('.header-nav, .header-nav__content, .header-menu-toggle, .header-menu-toggle span') ) {
                 siteBody.removeClass('menu-is-open');
             }
         });
-
     };
-
 
    /* Masonry
     * ---------------------------------------------------- */ 
     var ssMasonryFolio = function () {
-        
         var containerBricks = $('.masonry');
-
-        containerBricks.imagesLoaded(function () {
+        containerBricks.imagesLoaded(function ()
+        {
             containerBricks.masonry({
                 itemSelector: '.masonry__brick',
                 resize: true
             });
         });
     };
-
 
    /* photoswipe
     * ----------------------------------------------------- */
